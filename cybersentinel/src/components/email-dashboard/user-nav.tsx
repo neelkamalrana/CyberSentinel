7a95271e018672b5729faf4aca6a57408990da93
shoaibk7861 <sp05764n@pace.edu>
2025-05-07 05:08:33 -0400

    Update user-nav.tsx

--- cybersentinel/src/components/email-dashboard/user-nav.tsx
+++ cybersentinel/src/components/email-dashboard/user-nav.tsx
@@ -38,23 +38,7 @@ interface UserNavProps {
 
 export function UserNav({ user, onLogout }: UserNavProps) {
   return (
-    <div className="flex items-center space-x-4">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button variant="ghost" size="icon" className="relative">
-              <Bell className="h-5 w-5" />
-              <span className="absolute -top-1 -right-1 h-4 w-4 rounded-full bg-destructive text-[10px] text-white flex items-center justify-center">
-                3
-              </span>
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent className="bg-background border shadow-md">
-            <p>Notifications</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      
+    <div className="flex items-center space-x-4">      
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative flex items-center space-x-2 h-8 mr-2">
